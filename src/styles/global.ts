0a93af24a8cfaf9107cda73816527b85bfc3ac61
eduardo <eduardo.nobrega1999@hotmail.com>
2023-07-03 15:41:21 -0300

    global style

---
+++ src/styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  :focus-visible {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
+  }
+
+  *, *::before, *::after {
+      box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+  }
+
+
+  img, picture, svg, video {
+    display: block;
+    max-width: 100%
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.6rem;
+    font-weight: 400;
+    
+    -webkit-font-smoothing: antialiased; 
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-100']};
+  }
+`
