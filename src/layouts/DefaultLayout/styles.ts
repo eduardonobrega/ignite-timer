0f6f1c22fc132017a7f1095510af288582c60896
eduardo <eduardo.nobrega1999@hotmail.com>
2023-07-04 15:14:31 -0300

    add overflow

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -4,6 +4,8 @@ export const LayoutContainer = styled.div`
   background-color: ${({ theme }) => theme['gray-800']};
 
   width: min(95%, 112rem);
+  overflow: hidden;
+  max-height: 74rem;
   padding-bottom: 10vh;
   border-radius: 8px;
 `
